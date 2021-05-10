672c8a5d5a7abb25356655c62cfab3dae767f3c8
shijuvar <gophermonk@gmail.com>
2021-05-10 07:24:28 +0530

    Minor modification in JetStream demo

--- jsdemo/monitor/main.go
+++ jsdemo/monitor/main.go
@@ -19,7 +19,7 @@ func main() {
 	}
 	// Create durable consumer monitor
 	js.Subscribe("ORDERS.*", func(msg *nats.Msg) {
-		//msg.Ack()
+		msg.Ack()
 		var order model.Order
 		err := json.Unmarshal(msg.Data, &order)
 		if err != nil {
@@ -28,7 +28,7 @@ func main() {
 
 		log.Printf("monitor service subscribes from subject:%s\n", msg.Subject)
 		log.Printf("OrderID:%d, CustomerID: %s, Status:%s\n", order.OrderID, order.CustomerID, order.Status)
-	}, nats.Durable("monitor"))
+	}, nats.Durable("monitor"), nats.ManualAck())
 
 	runtime.Goexit()
 
